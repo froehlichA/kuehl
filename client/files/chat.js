bf0327b0925d08093af1f6edcf986296474b1064
Fröhlich A <froehlich.alexander@protonmail.com>
2017-09-28 23:33:49 +0200

    try automatic connecting

--- client/files/chat.js
+++ client/files/chat.js
@@ -3,7 +3,7 @@ var room = document.getElementById('room');
 var symbol = document.getElementById('symbol');
 var iframe = document.getElementById('iframe');
 var input = document.getElementById('input');
-var socket = io.connect('http://localhost:3000', { secure: true });
+var socket = io.connect('https://kuehlchat.herokuapp.com');
 
 joinRoom();
 
